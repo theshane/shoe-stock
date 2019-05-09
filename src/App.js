bc56c29ed62aae2a73e92d9cfad3434c77f8ed2d
Shane Burgess <shane.burgess@gmail.com>
2019-05-09 07:20:38 -0400

    Updates the app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Navbar from "./NavBar.js";
+import Container from "react-bootstrap/Container.js";
+import Row from "react-bootstrap/Row.js";
+import Col from "react-bootstrap/Col.js";
+import styled from "styled-components";
+import StockCard from "./StockCard.js";
+import FormModal from "./FormModal.js";
+
+const StyledContainer = styled(Container)`
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 30px;
+`;
 
 function App() {
+  const [stock, setStock] = useState({});
+  const [showModal, setShowModal] = useState(false);
+  const [currentShelfId, setCurrentShelfId] = useState(0);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("data.json").then(async response => {
+        const data = await response.json();
+        setStock(data);
+      });
+    };
+    fetchData();
+  }, []);
+
+  const updateStock = (shelfId, newValue) => {
+    newValue.location = shelfId;
+    newValue.hasStock = newValue.brand ? true : false;
+    stock[shelfId] = newValue;
+    setStock({ ...stock });
+    setShowModal(false);
+  };
+
+  console.log(currentShelfId, stock[currentShelfId]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FormModal
+        show={showModal}
+        shelfId={currentShelfId}
+        setShowModal={setShowModal}
+        updateStock={updateStock}
+        stock={stock[currentShelfId] || {}}
+      />
+      <Navbar />
+      <StyledContainer>
+        <Row>
+          {Object.keys(stock).map(location => {
+            return (
+              <Col>
+                <StockCard
+                  shelf={stock[location]}
+                  setShowModal={setShowModal}
+                  setCurrentShelfId={setCurrentShelfId}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </StyledContainer>
     </div>
   );
 }
