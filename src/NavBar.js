6a0cce188ccaf5346a148bb318a915ee95b3422c
Shane Burgess <shane.burgess@gmail.com>
2019-05-09 10:19:49 -0400

    Fixes console error on tests

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import {Navbar, Form, FormControl, Nav, Button} from 'react-bootstrap';
-import styled from "styled-components";
-const FullWidthNav = styled(Nav)`
-width: 100%;
-display: flex;
-`;
+
 
 
 function NavBar(props) {
   return (
     <Navbar bg="dark" expand="lg">
     <Navbar.Brand href="#home">Shoe Stock</Navbar.Brand>
-    <FullWidthNav className="justify-content-end">
+    <Nav>
     <Form inline>
-      <FormControl type="text" placeholder="(UPC,Size,Brand,Style)" className="mr-sm-2" value={props.filterText} onChange={(event) => {props.setFilterText(event.target.value);}}/>
+      <FormControl type="text" placeholder="(UPC,Size,Brand,Style)" value={props.filterText} onChange={(event) => {props.setFilterText(event.target.value);}}/>
       {props.filterText && <Button variant="secondary" onClick={() => {props.setFilterText('');}}>Clear</Button>}
     </Form>
- </FullWidthNav>
+ </Nav>
   </Navbar>
   );
 }
