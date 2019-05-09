d7a894b7bc4cc0cac01be4f5b62f425b0eca1850
Shane Burgess <shane.burgess@gmail.com>
2019-05-09 07:20:29 -0400

    Adds the navbar component

---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Navbar, Nav, Form, FormControl, Button, NavDropdown} from 'react-bootstrap';
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">Shoe Stock</Navbar.Brand>
+  </Navbar>
+  );
+}
+
+export default NavBar;
